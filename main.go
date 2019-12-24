69963a2dad7fe3eb0eb43885d9671f3c595cb421
bobodell <cscrodell@gmail.com>
2019-12-24 09:17:27 -0800

    upload

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "log"
+    "net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+    switch r.Method {
+    case "GET":
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte(`{"message": "get called"}`))
+    case "POST":
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(`{"message": "post called"}`))
+    case "PUT":
+        w.WriteHeader(http.StatusAccepted)
+        w.Write([]byte(`{"message": "put called"}`))
+    case "DELETE":
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte(`{"message": "delete called"}`))
+    default:
+        w.WriteHeader(http.StatusNotFound)
+        w.Write([]byte(`{"message": "not found"}`))
+    }
+}
+
+func main() {
+    http.HandleFunc("/", home)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
